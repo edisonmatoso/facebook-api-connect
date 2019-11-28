52c7ab3a47f7793e469c16b7fb5549db76bbd6ed
Edison Matoso <ejmatoso@gmail.com>
2019-11-28 13:32:51 -0300

    possibilitando a injeção por iframe

--- src/App.js
+++ src/App.js
@@ -4,27 +4,19 @@ import "./App.css";
 
 function App() {
   function handleFacebookData() {
-    const { api } = window.fbObj;
-    api("/me/accounts", function(response) {
-      response.data.map(({ access_token, id }) =>
-        api(
-          `/${id}/subscribed_apps?subscribed_fields=messages,message_echoes,messaging_policy_enforcement`,
-          "post",
-          { access_token },
-          response => console.log(response)
-        )
-      );
-    });
+    window.parent.handleFacebookData(window.fbObj.getAccessToken());
   }
 
   return (
     <FacebookLogin
-      appId={window.appId}
+      appId={window.parent.appId || window.appId}
       autoLoad={false}
       fields="name,email,picture"
       scope="manage_pages,pages_messaging"
       onClick={() => {}}
       callback={response => handleFacebookData(response)}
+      size="medium"
+      icon="fa-facebook"
     />
   );
 }
